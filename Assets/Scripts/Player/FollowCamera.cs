fd69d9066a2d20d17eda44d022bfac747cf517c5
YatharthChopra <yatharthchopra21@gmail.com>
2026-04-09 13:26:31 +0000

    Adjust camera to third-person angle

--- Assets/Scripts/Player/FollowCamera.cs
+++ Assets/Scripts/Player/FollowCamera.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     public Transform target;
-    public Vector3 offset = new Vector3(0f, 20f, -10f);
+    public Vector3 offset = new Vector3(0f, 8f, -10f);
     public float smoothSpeed = 8f;
 
     void LateUpdate()
